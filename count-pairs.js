2119325abb546116f2f3510f5e660db0af4c392f
Jason Benjoya <jasonbenjoya@gmail.com>
2021-05-25 15:18:09 -0500

    completed subsequence and pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target) {
+  const set = new Set(arr);
+  // we will return this counter
+  let count = 0;
+
+  for (let num of arr) {
+    set.delete(num);
+    if (set.has(target - num)) count++;
+  }
+  return count;
 }
+
+module.exports = { countPairs };
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(sub, main) {
+  let subIdx = 0;
+  let mainIdx = 0;
+  // if str1 is empty, return true
+
+  if (!sub) return true;
+  //   loop over entire main string
+  while (mainIdx < main.length) {
+    // compare each string at their respective index
+    if (main[mainIdx] === sub[subIdx]) {
+      // if so, increase the subindex;
+      subIdx++;
+    }
+    // if entirety of string one has been traversed, then all letters are in string 2 in the correct order so return true;
+    if (subIdx === sub.length) return true;
+
+    // go to next character in main
+    mainIdx++;
+  }
+  return false;
 }
+
+module.exports = { isSubsequence };
