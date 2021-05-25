e96d6785c92791ad20d1585332bd6a923a1e4d7b
Jason Benjoya <jasonbenjoya@gmail.com>
2021-05-25 15:05:03 -0500

    completed two-array, same-frequency, and separate positive

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,29 @@
+const makeCounter = str => {
+  const freqCounter = {};
+  for (let char of str) {
+    freqCounter[char] = freqCounter[char] + 1 || 1;
+  }
+  return freqCounter;
+};
+
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+  // convert to string to create frequency counter
+  const num1str = num1.toString();
+  const num2str = num2.toString();
+
+  //   test length of each string; if not even, not possible
+  if (num1str.length !== num2str.length) return false;
+
+  // make frequency counters
+  const num1Freq = makeCounter(num1str);
+  const num2Freq = makeCounter(num2str);
+
+  for (let digit in num1Freq) {
+    if (!num2Freq[digit]) return false;
+    if (num1Freq[digit] !== num2Freq[digit]) return false;
+  }
+  return true;
 }
+
+module.exports = { makeCounter, sameFrequency };
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,9 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+  return keys.reduce((output, curr, idx) => {
+    output[curr] = idx < values.length ? values[idx] : null;
+    return output;
+  }, {});
 }
+
+module.exports = { twoArrayObject };
