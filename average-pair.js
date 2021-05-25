0e80633e01e7dfac1556f07bafc7d014cab5ff04
Jason Benjoya <jasonbenjoya@gmail.com>
2021-05-25 14:40:13 -0500

    completed average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, target) {
+  // make pointers at start and end
+  let left = 0;
+  let right = arr.length - 1;
+
+  // run loop until the paths cross or the truthy answer is found
+  while (left < right) {
+    let avg = (arr[left] + arr[right]) / 2;
+    if (avg === target) {
+      return true;
+    } else if (avg < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
 }
+
+module.exports = { averagePair };
